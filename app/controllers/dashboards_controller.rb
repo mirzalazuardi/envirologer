d1e1176272d077d36b53e90d448766a98860844a
Mirzalazuardi Hermawan <mirzalazuardi@gmail.com>
2016-08-04 14:36:36 +0700

    value dashboard from last aqmesh data

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,7 @@
 class DashboardsController < ApplicationController
   before_action :authenticate_user!
   def index
-    idDatum = AqmeshDatum.last.id
-    @channels = AqmeshChannel.find_by_aqmesh_datum_id(idDatum)
+
+    @channels = AqmeshDatum.last.aqmesh_channels
   end
 end
