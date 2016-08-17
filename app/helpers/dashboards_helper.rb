2729b36ac69d754fd4fde09002111c038edc5ce7
mirzalazuardi <mirzalazuardi@gmail.com>
2016-08-17 22:02:02 +0700

    fix indicator

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -7,34 +7,30 @@ module DashboardsHelper
     comp
   end
 
-  def indicator_color(*args)
-    label = args[0]
-    value = args[1]
-    bad_to_good_color = ['black','red','yellow','green','blue']
+  def set_indicator_for(*args)
+    label,value = *args
 
-    #if label.upcase == 'TEMP'
-      #hn = 31
-    #elsif label.upcase == 'HUM'
-      #hn = 76
-    #elsif label.upcase == 'AP'
-      #hn = 1000
-    #elsif label.upcase == 'O3'
-      #hn = 22
-    #elsif label.upcase == 'SO2'
-      #hn = 500
-    #elsif label.upcase == 'NO2'
-      #hn = 20
-    #elsif label.upcase == 'NO'
-      #hn = 3
-    #end
-    hn = 1020
+    if label.upcase == 'O3'
+      indicator_color(value,51,101,169,209,748)
+    elsif label.upcase == 'SO2'
+      indicator_color(value,41,81,381,801,1600)
+    elsif label.upcase == 'CO' 
+      indicator_color(value,1.1,2.1,10,17,34)
+    end
+  end
+
+  def indicator_color(*args)
+    value,kpi1,kpi2,kpi3,kpi4,kpi5 = args
+    bad_to_good_color = ['black','purple','red','yellow','green','blue']
 
     case value
-    when set_min_max_from(hn,5.0,5)['min']..set_min_max_from(hn,5,5)['max'] then result = bad_to_good_color[0]
-    when set_min_max_from(hn,5.0,4)['min']..set_min_max_from(hn,5,4)['max'] then result = bad_to_good_color[1]
-    when set_min_max_from(hn,5.0,3)['min']..set_min_max_from(hn,5,3)['max'] then result = bad_to_good_color[2]
-    when set_min_max_from(hn,5.0,2)['min']..set_min_max_from(hn,5,2)['max'] then result = bad_to_good_color[3]
-    when set_min_max_from(hn,5.0,1)['min']..set_min_max_from(hn,5,1)['max'] then result = bad_to_good_color[4] 
+    when 0..(kpi1.to_i-1) then result = bad_to_good_color[5]
+    when kpi1..(kpi2.to_i-1) then result = bad_to_good_color[4]
+    when kpi2..(kpi3.to_i-1) then result = bad_to_good_color[3]
+    when kpi3..(kpi4.to_i-1) then result = bad_to_good_color[2]
+    when kpi4..(kpi5.to_i-1) then result = bad_to_good_color[1] 
+    else 
+      result = bad_to_good_color[0]
     end
     result
   end
