337756ca8e3b3fe50f38b1af33b5a1e1e8a3b1ca
mirzalazuardi <mirzalazuardi@gmail.com>
2016-09-03 18:04:03 +0700

    update json latest url (demo id, 67)

--- app/controllers/publicvars_controller.rb
+++ app/controllers/publicvars_controller.rb
@@ -1,9 +1,12 @@
 class PublicvarsController < ApplicationController
   def latest
-    dt = AqmeshDatum.last
-    latest_value = dt.aqmesh_channels.map { |x| [x.label,x.final] } 
+    station = Station.where(number: params[:station_number]).first
+    dt = AqmeshDatum.where(station_id: station.id).last
+    latest_value = dt.aqmesh_channels.map { |x| [x.label,"%4.2f %s" % [x.final,x.unit]] } 
     @public_latest = {}
     @public_latest[:timestamp] = dt.timestamp
+    @public_latest[:latitude] = dt.station.latitude
+    @public_latest[:longitude] = dt.station.longitude
     @public_latest[:data] = latest_value
     render json: @public_latest
   end
